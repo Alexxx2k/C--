f87b5fb87f64d441063ffde430f672eecb97d2ac
AlexNik <sahan@DESKTOP-R8OTJOJ>
2024-02-19 19:12:10 +0300

    add

---
+++ first/header.hpp
@@ -0,0 +1,10 @@
+#ifndef HEADER_HPP
+#define HEADER_HPP
+
+void printf()
+{
+  std::cout << "Hello World!";
+}
+
+#endif 
+
--- first/main.cpp
+++ first/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include "header.hpp";
 
 int main()
 {
   int i = 0;
+  i += 1;
   std::cout << i;
-  std::cout << i;
+  std::cout << i << "\n";
+  printf();
   return 0;
+
 }
